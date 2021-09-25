23be238e0c67ed551603fa2b939a871d272013d2
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-09-25 12:30:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById("1b").addEventListener('click', function() {
+    var aud = new Audio('sounds/firework.mp3')
+    aud.play();
+})
+
+document.getElementById("2b").addEventListener('click', function() {
+    var aud = new Audio('sounds/firework.mp3')
+    aud.play();
+})
+
+document.getElementById("3b").addEventListener('click', function() {
+    var aud = new Audio('sounds/firework.mp3')
+    aud.play();
+})
+document.getElementById("4b").addEventListener('click', function() {
+    var aud = new Audio('sounds/firework.mp3')
+    aud.play();
+})
+document.getElementById("5b").addEventListener('click', function() {
+    var aud = new Audio('sounds/firework.mp3')
+    aud.play();
+})
